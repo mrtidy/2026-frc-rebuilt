7a76f25094a5a3e27be5e61a0124f70610ada53c
Brandon Martinez <me@brandonmartinez.com>
2026-01-15 18:03:49 -0500

    Created the skeleton for the turret subsystem

---
+++ src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.turret;
+
+import frc.robot.config.TurretSubsystemConfig;
+import frc.robot.subsystems.AbstractSubsystem;
+
+public class TurretSubsystem extends AbstractSubsystem<TurretSubsystemConfig> {
+    public TurretSubsystem(TurretSubsystemConfig config) {
+        super(config);
+    }
+    
+}
